fb78f606d52f7ff2719c3c9f4a0febace50c5a43
zhaoxin <zhaoxin@meishubao.com>
2020-03-19 19:43:37 +0800

    Add Location.gpx

--- ZXToolboxDemo/ViewController.m
+++ ZXToolboxDemo/ViewController.m
@@ -19,6 +19,13 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     self.view.backgroundColor = UIColor.whiteColor;
+
+    ZXCoordinate2D baidu = ZXCoordinate2DMake(40.02009, 116.467418);
+    ZXCoordinate2D china = ZXCoordinate2DMake(40.02009, 116.467418);
+    ZXCoordinate2D world = ZXCoordinate2DChinaToWorld(china);
+    NSLogA(@"#WGS-84: %f, %f", world.latitude, world.longitude);
+    NSLogA(@"#GCJ-02: %f, %f", china.latitude, china.longitude);
+    NSLogA(@"#BD-09: %f, %f", baidu.latitude, baidu.longitude);
 }
 
 
